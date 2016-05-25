a858588054ad78ef3cfd1e5195e4d6e7ff3eb369
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-25 14:21:21 -0400

    made it so index accesses array

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <!DOCTYPE HTML>
 	
 	<head>
-		<ink type="text/css" rel="user-array" href="user-array.php"/>
 		<title>List PHP</title>
 	</head>
 
 	<body>
 		<?php
+
+			$user_array = include("user-array.php");
+
 			echo "hi";
+
 			foreach ($user_array as $theArray) {
 				echo "how is it going";
 				foreach ($theArray as $theInfo) {
 					echo "$theInfo";
 				}
 			}
+			unset($theArray);
+			unset($theInfo);
 		?>
 	</body>
 
