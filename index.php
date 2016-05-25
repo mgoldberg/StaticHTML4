a1a96d7633e671af85745dbafdd4f937d576ae77
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-25 13:52:52 -0400

    added array file

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <!DOCTYPE HTML>
 	
+	<head>
+		<link type="text/css" rel="user-array" href="user-array.php"/>
+		<title>List PHP</title>
+	</head>
+
 </HTML>
---
+++ user-array.php
@@ -0,0 +1,58 @@
+<?php
+
+$user_array = array(
+	array(
+		'first_name'    => 'Dustyn',
+		'last_name'     => 'Doyle',
+		'age'           => 25,
+		'occupation'    => 'Developer'
+	),
+	array(
+		'first_name'    => 'Tim',
+		'last_name'     => 'Corr',
+		'age'           => 27,
+		'occupation'    => ''
+	),
+	array(
+		'first_name'    => 'Ryan',
+		'last_name'     => 'Lebo',
+		'age'           => 24,
+		'occupation'    => 'Developer'
+	),
+	array(
+		'first_name'    => 'Nick',
+		'last_name'     => 'Belcher',
+		'age'           => 25,
+		'occupation'    => 'Creative'
+	),
+	array(
+		'first_name'    => 'Adam',
+		'last_name'     => '',
+		'age'           => 26,
+		'occupation'    => 'Creative'
+	),
+	array(
+		'first_name'    => 'Chris',
+		'last_name'     => 'McConney',
+		'age'           => 33,
+		'occupation'    => 'Project Manager'
+	),
+	array(
+		'first_name'    => 'Megan',
+		'last_name'     => 'Langdon',
+		'age'           => '',
+		'occupation'    => 'Project Coordinator'
+	),
+	array(
+		'first_name'    => 'Cory',
+		'last_name'     => 'Embody',
+		'age'           => 29,
+		'occupation'    => 'Administrative Manager'
+	),
+	array(
+		'first_name'    => 'Megan',
+		'last_name'     => 'Miller',
+		'age'           => 28,
+		'occupation'    => 'SalesBuddy Pro'
+	)
+);
