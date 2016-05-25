44f0d0cd56c084e9ea49f9fec747a32b1162f3a3
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-25 14:04:35 -0400

    started loop, buggy

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <!DOCTYPE HTML>
 	
 	<head>
-		<link type="text/css" rel="user-array" href="user-array.php"/>
+		<ink type="text/css" rel="user-array" href="user-array.php"/>
 		<title>List PHP</title>
 	</head>
+
+	<body>
+		<?php
+			echo "hi";
+			foreach ($user_array as $theArray) {
+				echo "how is it going";
+				foreach ($theArray as $theInfo) {
+					echo "$theInfo";
+				}
+			}
+		?>
+	</body>
 
 </HTML>
