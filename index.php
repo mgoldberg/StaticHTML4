101bed48f55058ca83fc2ffb766ad8bbca5ef90e
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-25 15:52:51 -0400

    moved loop into a function in a different file

--- index.php
+++ index.php
@@ -6,47 +6,10 @@
 
 	<body>
 		<?php
-
 			include'user-array.php';
+			include 'UserFunctionality.php';
 
-			foreach ($user_array as $theArray) {
-				for($i = 0; $i < 4; $i++) {
-					if($i == 0){
-						if($theArray[first_name] != null){
-							echo "--First Name: "."$theArray[first_name]"." ";
-						}
-						else{
-							echo "--First Name: Data not found ";
-						}
-					}
-					else if($i == 1){
-						if($theArray[last_name] != NULL){
-							echo "--Last Name: "."$theArray[last_name]"." ";
-						}
-						else{
-							echo "--Last Name: Data not found ";
-						}
-					}
-					else if($i == 2){
-						if($theArray[age] != NULL){
-							echo "--Age: "."$theArray[age]"." ";
-						}
-						else{
-							echo "--Age: Data not found ";
-						}
-					}
-					else if($i == 3){
-					if($theArray[occupation] != NULL){
-							echo "--Occupation: "."$theArray[occupation]"." <br><br>";
-						}
-						else{
-							echo "--Occupation: Data not found  <br><br>";
-						}
-					}
-				}
-			}
-			unset($theArray);
-			unset($theInfo);
+			displayArray();
 		?>
 	</body>
 
