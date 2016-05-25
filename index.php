3245493ea01c3913565b65761daedb2531b43a95
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-25 15:26:11 -0400

    check if inforamtion is missing and added a bit of seperation

--- index.php
+++ index.php
@@ -9,20 +9,39 @@
 
 			include'user-array.php';
 
-
 			foreach ($user_array as $theArray) {
 				for($i = 0; $i < 4; $i++) {
 					if($i == 0){
-						echo "$theArray[first_name]"." ";
+						if($theArray[first_name] != null){
+							echo "--First Name: "."$theArray[first_name]"." ";
+						}
+						else{
+							echo "--First Name: Data not found ";
+						}
 					}
 					else if($i == 1){
-						echo "$theArray[last_name]"." ";
+						if($theArray[last_name] != NULL){
+							echo "--Last Name: "."$theArray[last_name]"." ";
+						}
+						else{
+							echo "--Last Name: Data not found ";
+						}
 					}
 					else if($i == 2){
-						echo "$theArray[age]"." ";
+						if($theArray[age] != NULL){
+							echo "--Age: "."$theArray[age]"." ";
+						}
+						else{
+							echo "--Age: Data not found ";
+						}
 					}
 					else if($i == 3){
-						echo "$theArray[occupation]"." ";
+					if($theArray[occupation] != NULL){
+							echo "--Occupation: "."$theArray[occupation]"." - - - - ";
+						}
+						else{
+							echo "--Occupation: Data not found  - - - - ";
+						}
 					}
 				}
 			}
