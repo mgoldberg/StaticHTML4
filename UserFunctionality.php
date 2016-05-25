89d1ef8f538fe0cf62910540af5d956bf8872d9f
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-25 16:13:49 -0400

    un-hard coded loop and checked for tag

--- UserFunctionality.php
+++ UserFunctionality.php
@@ -5,28 +5,14 @@
 		{
 			include'user-array.php';
 
+			error_log(print_r($user_array, true));
+
 			foreach ($user_array as $theArray) {
-				for($i = 0; $i < 4; $i++) {
-					if($i == 0){
-						if(!empty( $theArray[first_name] ) ){
-							echo "--First Name: "."$theArray[first_name]"." ";
-						}
+				foreach($theArray as $key => $value) {
+					if(!empty($value)){
+						echo $key."-->".$value."<br>";
 					}
-					else if($i == 1){
-						if($theArray[last_name] != NULL){
-							echo "--Last Name: "."$theArray[last_name]"." ";
-						}					}
-					else if($i == 2){
-						if($theArray[age] != NULL){
-							echo "--Age: "."$theArray[age]"." ";
-						}
-					}
-					else if($i == 3){
-						if($theArray[occupation] != NULL){
-							echo "--Occupation: "."$theArray[occupation]"." <br><br>";
-						}
-					}
-				}
+				}	
 			}
 			unset($theArray);
 			unset($theInfo);
